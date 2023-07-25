8f485af9b0bc212d06b925a67d0e147ca8316589
Andrew Buter <andrey.buter@gmail.com>
2023-07-25 23:28:46 +0200

    remove comment

--- src/app/web-worker/web-worker-factory.service.ts
+++ src/app/web-worker/web-worker-factory.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WebWorkerFactoryService {
-  // jest tests fix - https://github.com/kulshekhar/ts-jest/issues/1174#issuecomment-1223067199
   getWorker() {
     return new Worker(new URL('./app.worker', import.meta.url), { type: 'module' });
   }
